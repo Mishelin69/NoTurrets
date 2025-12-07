8911adcafa9f7ec58376b5dc4c5deb734de1c112
Mishelin69 <kosecmilos6@gmail.com>
2025-12-07 14:48:26 +0100

    v1.0

---
+++ NoTurrets.cs
@@ -0,0 +1,84 @@
+using System.Linq;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LobbyCompatibility.Attributes;
+using LobbyCompatibility.Enums;
+
+namespace NoTurrets;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("BMX.LobbyCompatibility", BepInDependency.DependencyFlags.HardDependency)]
+[LobbyCompatibility(CompatibilityLevel.Everyone, VersionStrictness.None)]
+public class NoTurrets : BaseUnityPlugin
+{
+    public static NoTurrets Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger { get; private set; } = null!;
+    internal static Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Instance = this;
+
+        Patch();
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+    }
+
+    private static void PluginLogInfoWithPrefix(string content)
+    {
+        Logger.LogInfo($"[EVIL] ${content}");
+    }
+
+    [HarmonyPatch(typeof(RoundManager))]
+    internal class TurretPatch
+    {
+        [HarmonyPatch("SpawnMapObjects")]
+        [HarmonyPrefix]
+        static void RemoveAllTurrets(RoundManager __instance)
+        {
+            if (__instance.currentLevel.spawnableMapObjects.Length <= 0)
+            {
+                PluginLogInfoWithPrefix("No enemies to spawn!");
+                return;
+            }
+
+            PluginLogInfoWithPrefix("Trying to remove turrets from the game...");
+            var spawnableEnemies = __instance.currentLevel.spawnableMapObjects.ToList();
+            int originalListLength = spawnableEnemies.Count;
+
+            spawnableEnemies.RemoveAll(x => x.prefabToSpawn.name.Contains("Turret"));
+            __instance.currentLevel.spawnableMapObjects = spawnableEnemies.ToArray();
+
+            if (spawnableEnemies.Count != originalListLength)
+            {
+                PluginLogInfoWithPrefix($"Successfully removed turrets from the game yay! {originalListLength} != {spawnableEnemies.Count}");
+            }
+            else
+            {
+                PluginLogInfoWithPrefix("No turrets were needed to be removed!");
+            }
+        }
+    }
+
+    internal static void Patch()
+    {
+        Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        Logger.LogDebug("Patching...");
+
+        Harmony.PatchAll();
+
+        Logger.LogDebug("Finished patching!");
+    }
+
+    internal static void Unpatch()
+    {
+        Logger.LogDebug("Unpatching...");
+
+        Harmony?.UnpatchSelf();
+
+        Logger.LogDebug("Finished unpatching!");
+    }
+}
---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DLL_NAME="mishelin.NoTurrets.dll"
+#DEST_PATH="your-destination-path"
+BUILD_OUTPUT="./bin/Release/netstandard2.1"
+ZIP_NAME="NoTurrets_Release.zip"
+
+dotnet build --configuration Release
+
+if [ $? -eq 0 ]; then
+    echo "--- Build Successful. Moving files... ---"
+
+    # Create destination directory if it doesn't exist
+    mkdir -p "$DEST_PATH"
+
+    # uncoment this if you want to copy the dll into somewhere
+    #$ cp "$BUILD_OUTPUT/$DLL_NAME" "$DEST_PATH/"
+    zip -j "$ZIP_NAME" "$BUILD_OUTPUT/$DLL_NAME" "$BUILD_OUTPUT/manifest.json" "README.md" "icon.png"
+else
+    echo "!!! Build Failed. Aborting move. !!!"
+    exit 1
+fi
